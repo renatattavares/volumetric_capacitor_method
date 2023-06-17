c67a2b9521528fb3988734eadc3012ff927d7f39
Renata Tavares <renatattavares@hotmail.com>
2023-06-17 18:10:44 -0300

    Liquid fraction convergence loop was added

--- VCM2D/VCM2D.cpp
+++ VCM2D/VCM2D.cpp
@@ -4,10 +4,131 @@
 #include <fstream>
 #include <string>
 #include <time.h>
+#include "VCM2D.h"
 
 using namespace std;
 
 #pragma warning(disable:4996)
+
+int main() {
+
+	// Output file info
+	char ffn[40];
+
+	// ---------------------------------- --------------------------------//
+	// Cell properties
+	double D = 0.018; // Battery cell diameter [m]
+	double h = 0.065; // Battery cell height [m]
+
+	// Mesh Properties
+	const int Nx = 10; // Volumes in x direction
+	const int Ny = 10; // Volumes in y direction 
+	double dx = 0.005; // CV dimension in x direction [m]
+	double dy = 0.005; // CV dimension in y direction [m]
+	const int N = (int)Nx * Ny; // Total number of CVs
+	double dt = 2;	   // Time step [s] 
+	double time = dt;
+	bool fconverged = false;
+	double fresmax = 1E-3;
+	int i = 0;
+
+	// ---------------------------------- --------------------------------//
+	// Allocating memory and cleaning any previously stored value
+	double* fres = (double*)malloc((N) * sizeof(double));
+	double* ap = (double*)malloc((N) * sizeof(double));
+	double* ae = (double*)malloc((N) * sizeof(double));
+	double* aw = (double*)malloc((N) * sizeof(double));
+	double* an = (double*)malloc((N) * sizeof(double));
+	double* as = (double*)malloc((N) * sizeof(double));
+	double* Ti = (double*)malloc((N) * sizeof(double));
+	double* b = (double*)malloc((N) * sizeof(double));
+	double* s = (double*)malloc((N) * sizeof(double));
+	double* T = (double*)malloc((N) * sizeof(double));
+	int* fi = (int*)malloc((N) * sizeof(int));
+	int* f = (int*)malloc((N) * sizeof(int));
+	int* R = (int*)malloc((N) * sizeof(int));
+
+	for (int i = 0; i < N; i++) {
+
+		// Floats
+		fres[i] = 0.0;
+		ap[i] = 0.0;
+		ae[i] = 0.0;
+		an[i] = 0.0;
+		as[i] = 0.0;
+		aw[i] = 0.0;
+		Ti[i] = 15;
+		T[i] = 15;
+		b[i] = 0.0;
+		s[i] = 0.0;
+		T[i] = 0.0;
+		// Integers
+		fi[i] = 0;
+		f[i] = 0;
+		R[i] = 0;
+
+	}
+	// ---------------------------------- --------------------------------//
+	// Physical properties
+	// Selected PCM: RT 18 HC [T_melt = 18°C, Latent heat of fusion = 250 kJ/kg, Cp = 2 kJ/kgK, k = 2 kJ/kgK, rho = 880 <-> 770 kg/m³ (solid/liquid)]
+
+	double Tmelt = 18;			 // PCM melting temperature [°C]
+	double rho_pcm_solid = 880;  // PCM density in solid state [kg/m³]
+	double rho_pcm_liquid = 770; // PCM density in liquid state [kg/m³]
+	double k_bat = 1000.0;       // Battery effective thermal conductivity [W/m.K]
+	double k_pcm = 2.0;          // PCM thermal conductivity [W/m.K]
+	double cp = 2;				 // PCM specific heat at constante pressure [kJ/kg.K]
+	double L = 250;				 // PCM latent heat of fusion [kJ/kg]
+	double TotalTime = 1200;		 // Total simulation time [s]
+	
+
+	// ---------------------------------- --------------------------------//
+	// Temperatures for boundary conditions
+	double Tn = 0.0; // North
+	double Ts = 0.0; // South 
+	double Te = 0.0; // East
+	double Tw = 0.0; // West
+
+	// Heat flux for boundary conditions
+	double qn = 0.0; // North
+	double qs = 0.0; // South 
+	double qe = 0.0; // East
+	double qw = 0.0; // West
+	// ---------------------------------- --------------------------------//
+
+	// -------------------------- SIMULATION CODE ------------------------//
+	mesh_regions(D, dx, dy, Nx, Ny, R);
+	plot_regions(Nx, Ny, dx, dy, ffn, R, T);
+	
+	while (time <= TotalTime) {
+
+		fconverged = false;
+
+		while (fconverged == false) {
+
+			assembly(Nx, Ny, dx, dy, R, ap, ae, aw, an, as, s, cp, L, T, Ti, b, dt);
+			SOR(Nx, Ny, dx, dy, R, ap, ae, aw, an, as, s, k_bat, k_pcm, rho_pcm_solid, rho_pcm_liquid, cp, L, T, Ti, Tmelt, b, dt, f, fi, fres);
+
+			fconverged = areAllElementsSmaller(fres, N, fresmax);
+
+		}
+
+		printf("// ----- Solution for Time = %f ----- //\n", time);
+
+		for (i = 0; i < N; i++) {
+			
+			
+			printf("T%i = \t %f\n", i, T[i]);
+
+		}
+
+		time = time + dt;
+
+	}
+	
+	return 0;
+
+}
 
 void mesh_regions(double D, double dx, double dy, int Nx, int Ny, int* R) {
 
@@ -234,8 +355,8 @@ void assembly(int Nx, int Ny, double dx, double dy, int* R, double* ap, double* 
 	double ks = 0.0;
 	int f = 0;
 	int fi = 0;
-	
-		 
+
+
 	for (i = 0; i < Nx; i++)
 		for (j = 0; j < Ny; j++) {
 
@@ -248,7 +369,7 @@ void assembly(int Nx, int Ny, double dx, double dy, int* R, double* ap, double* 
 			find_k(dx, dy, R, o, ow, oe, on, os, kp, kw, ke, kn, ks);
 			find_rho(o, rho, rhoi, T, Ti);
 			find_f(o, f, fi, T, Ti);
-			
+
 			if (R[o] == 1) {
 
 				s[o] = 50000;
@@ -338,7 +459,7 @@ void output(int Nx, int Ny, double dx, double dy, int* R, char ffn[20]) {
 
 void SOR(int Nx, int Ny, double dx, double dy, int* R, double* ap, double* ae, double* aw, double* an, double* as, double* s,
 	double k_bat, double k_pcm, double rho_pcm_solid, double rho_pcm_liquid, double cp, double L, double* T, double* Ti, double Tmelt,
-	double* b, double dt) {
+	double* b, double dt, int* f, int* fi, double* fres) {
 
 	int i, j, o, ow, oe, on, os, N = 0;
 	double RR = 0.0;
@@ -370,122 +491,29 @@ void SOR(int Nx, int Ny, double dx, double dy, int* R, double* ap, double* ae, d
 
 			}
 
-		printf("Iteracao = %i\t Residuo = %13.5E\n", it, res);
+		//printf("Iteracao = %i\t Residuo = %13.5E\n", it, res);
 		it++;
 	}
 
-	printf("\nSolucao:\n");
 
-	N = Nx * Ny;
 
-	for (i = 0; i < N; i++) {
+	for (i = 0; i < Nx; i++)
+		for (j = 0; j < Ny; j++) {
 
-		printf("T%i = \t %f\n", i, T[i]);
+			o = (i * Ny) + j;
 
-	}
+			find_f(o, f[o], fi[o], T, Ti);
 
-	
+			fres[o] = fres[o] + abs(fi[o] - f[o]);
+
+		}
 }
 
-int main() {
-
-	// Output file info
-	char ffn[40];
-
-	// ---------------------------------- --------------------------------//
-	// Cell properties
-	double D = 0.018; // Battery cell diameter [m]
-	double h = 0.065; // Battery cell height [m]
-
-	// Mesh Properties
-	const int Nx = 5; // Volumes in x direction
-	const int Ny = 5; // Volumes in y direction 
-	double dx = 0.005; // CV dimension in x direction [m]
-	double dy = 0.005; // CV dimension in y direction [m]
-	const int N = (int)Nx * Ny; // Total number of CVs
-	double dt = 1;	   // Time step [s] 
-
-	// ---------------------------------- --------------------------------//
-	// Allocating memory and cleaning any previously stored value
-	double* ap = (double*)malloc((N) * sizeof(double));
-	double* ae = (double*)malloc((N) * sizeof(double));
-	double* aw = (double*)malloc((N) * sizeof(double));
-	double* an = (double*)malloc((N) * sizeof(double));
-	double* as = (double*)malloc((N) * sizeof(double));
-	double* Ti = (double*)malloc((N) * sizeof(double));
-	double* b = (double*)malloc((N) * sizeof(double));
-	double* s = (double*)malloc((N) * sizeof(double));
-	double* T = (double*)malloc((N) * sizeof(double));
-	int* fi = (int*)malloc((N) * sizeof(int));
-	int* f = (int*)malloc((N) * sizeof(int));
-	int* R = (int*)malloc((N) * sizeof(int));
-
-	for (int i = 0; i < N; i++) {
-
-		// Floats
-		ap[i] = 0.0;
-		ae[i] = 0.0;
-		an[i] = 0.0;
-		as[i] = 0.0;
-		aw[i] = 0.0;
-		Ti[i] = 15;
-		b[i] = 0.0;
-		s[i] = 0.0;
-		T[i] = 0.0;
-		// Integers
-		fi[i] = 0;
-		f[i] = 0;
-		R[i] = 0;
-
+bool areAllElementsSmaller(double* ptr, int size, double value) {
+	for (int i = 0; i < size; ++i) {
+		if (ptr[i] >= value) {
+			return false; // If any element is not smaller, return false
+		}
 	}
-	// ---------------------------------- --------------------------------//
-	// Physical properties
-	// Selected PCM: RT 18 HC [T_melt = 18°C, Latent heat of fusion = 250 kJ/kg, Cp = 2 kJ/kgK, k = 2 kJ/kgK, rho = 880 <-> 770 kg/m³ (solid/liquid)]
-
-	double Tmelt = 18;			 // PCM melting temperature [°C]
-	double rho_pcm_solid = 880;  // PCM density in solid state [kg/m³]
-	double rho_pcm_liquid = 770; // PCM density in liquid state [kg/m³]
-	double k_bat = 1000.0;       // Battery effective thermal conductivity [W/m.K]
-	double k_pcm = 2.0;          // PCM thermal conductivity [W/m.K]
-	double cp = 2;				 // PCM specific heat at constante pressure [kJ/kg.K]
-	double L = 250;				 // PCM latent heat of fusion [kJ/kg]
-	double TotalTime = 120;		 // Total simulation time [s]
-	double time = 0.0;
-
-	// ---------------------------------- --------------------------------//
-	// Temperatures for boundary conditions
-	double Tn = 0.0; // North
-	double Ts = 0.0; // South 
-	double Te = 0.0; // East
-	double Tw = 0.0; // West
-
-	// Heat flux for boundary conditions
-	double qn = 0.0; // North
-	double qs = 0.0; // South 
-	double qe = 0.0; // East
-	double qw = 0.0; // West
-	// ---------------------------------- --------------------------------//
-
-	// -------------------------- SIMULATION CODE ------------------------//
-	mesh_regions(D, dx, dy, Nx, Ny, R);
-	plot_regions(Nx, Ny, dx, dy, ffn, R, T);
-	
-	
-	while (time < TotalTime) {
-
-		
-
-		SOR(Nx, Ny, dx, dy, R, ap, ae, aw, an, as, s, k_bat, k_pcm, rho_pcm_solid, rho_pcm_liquid, cp, L, T, Ti, Tmelt, b, dt);
-		assembly(Nx, Ny, dx, dy, R, ap, ae, aw, an, as, s, cp, L, T, Ti, b, dt);
-		time = time + dt;
-
-	}
-
-	// ---------------------------------- --------------------------------//
-	
-	
-	
-	
-	return 0;
-
+	return true; // All elements are smaller
 }
